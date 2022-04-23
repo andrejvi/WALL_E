1de7bc7e28d9eef45fa804612d1dd1cac5da2160
raflemakt <raflemakt@hotmail.com>
2022-04-24 01:26:55 +0200

    Oppdaterte pythonskript til ny pakke

--- raspberry_pi/rpi_to_ESP32_serial_tool.py
+++ raspberry_pi/rpi_to_ESP32_serial_tool.py
@@ -1,15 +1,29 @@
 import struct
 import serial
 import time
+import os
 
 """ Pythonskript for å teste seriekommunikasjon mellom RPi (eller pc) og ESP32/Arduino
             prosjektarbeid gruppe 22, IELET1002 vår 2022 """
 
 
-PORT = "/dev/ttyUSB0"
+if os.name == "posix":
+    PORT = "/dev/ttyUSB0"
+else:
+    PORT = "COM"
+
 BAUD = 115200
 
 ser = serial.Serial(PORT, BAUD)
+pkg_datatypes = "<BBBBBBHHeBBBB"    # "B": uint8_t, "H": uint16_t, "e": 16float, "<": little endian encoded
+
+
+def get_packed_data(state=0, Kp=0, Ki=0, Kd=0, battery=0, speed=0, distance=0, u_state=0, u_Kp=0, u_Ki=0, u_Kd=0):
+    start_byte = ord("<")
+    stop_byte = ord(">")
+    data_package = struct.pack(pkg_datatypes, start_byte, state, Kp, Ki, Kd, battery, speed, distance, u_state, u_Kp, u_Ki, u_Kd, stop_byte)
+    return data_package
+
 
 def input_state_request():
     print("Skriv 'quit' for å avslutte")
@@ -18,7 +32,8 @@ def input_state_request():
         if requested_state.isdigit():
             requested_state = int(requested_state)
             if requested_state <= 255:
-                data_package = struct.pack("<B", requested_state)
+                data_package = get_packed_data(state=requested_state, u_state=1)
+                print(data_package)
                 ser.write(data_package)
         elif requested_state == "quit":
             break
@@ -30,7 +45,7 @@ def stress_test():
     delay_time = int(input("delaytid [ms]: "))
     for i in range(1000):
         for i in range(0, 12):
-            data_package = struct.pack("<B", i)
+            data_package = get_packed_data(state=i, u_state=1)
             ser.write(data_package)
             time.sleep(delay_time/1000)
 
